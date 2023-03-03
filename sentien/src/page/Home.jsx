22f80e9f3617bb60bb6e59e7241f02999fa44e0e
Aditya Raj <aadiyadav56837492@gmail.com>
2023-03-03 19:12:24 +0530

    sentien

--- sentien/src/page/Home.jsx
+++ sentien/src/page/Home.jsx
@@ -13,7 +13,8 @@ import { Input } from "@chakra-ui/react";
 const Home = () => {
   const [inputtext, setInput] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const [genre,setGenre]=useState([9])
+  const [genre,setGenre]=useState("")
+  const [rate,setRate]=useState(20)
 
   let data = [
     { title: "The Matrix", rating: 7.5, category: "Action" },
@@ -51,10 +52,25 @@ const Home = () => {
     setSearchResults(results);
   }, 300);
 
-  function handlegenre(i){
-   setGenre([...genre,i])
+  function handlegenre(item,category){
+   
+   setGenre(item)
+     let genredata=data.filter((item)=>{
+       return item.category===category
+     })
+setSearchResults(genredata)
+  
   }
 
+  function handlerating(i){
+     setRate(i)
+     let rating=data.filter((item)=>{
+      return item.rating>=i
+     })
+     setSearchResults(rating)
+  }
+
+  console.log(genre)
   useEffect(() => {
     handlesearch();
   }, [inputtext]);
@@ -91,14 +107,14 @@ const Home = () => {
                 .fill(0)
                 .map((item, i) => (
                   <Flex key={i}>
-                    <input type="checkbox" />
+                    <input type="checkbox" checked={rate===i}  onChange={()=>handlerating(i)} />
                     {Array(10)
                       .fill(0)
                       .map((item, k) => (
                         <Flex key={k} lineHeight={"25px"}>
                           {k <= i ? (
                             <h2 style={{ color: "black", fontSize: "50px" }}>
-                              &#11089;
+                              &#11089; 
                             </h2>
                           ) : (
                             <h2 style={{ color: "black", fontSize: "50px" }}>
@@ -125,7 +141,7 @@ const Home = () => {
             <AccordionPanel pb={4}>
               {data.map((item, i) => (
                 <Flex gap="10px" key={i}>
-                  <input type="checkbox" checked={genre===i} onClick={()=>handlegenre(i)}/>
+                  <input type="checkbox" checked={genre===i}  onChange={()=>handlegenre(i,item.category)}/>
                   <h1>{item.category}</h1>
                 </Flex>
               ))}
@@ -137,7 +153,14 @@ const Home = () => {
       {searchResults.length > 0 ? (
         <Box padding={"20px"} border="1px solid black" width={["25%"]}>
           {searchResults.map((item, i) => (
-            <div key={i}>{item.title}</div>
+            <div key={i} >
+              <div style={{backgroundColor:"gray",marginBottom:"10px",padding:"5px"}}>
+              <h2> title : {item.title}</h2>
+              <h2>rating :{item.rating}</h2>
+               <h2>Category :{item.category}</h2>
+              </div>
+         
+              </div>
           ))}
         </Box>
       ) : (
